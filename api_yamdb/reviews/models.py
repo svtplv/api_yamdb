195e58443a08bf7b340fcaa777f11d119c4e38ee
KseniiaSedova <sedovakseniya1506@gmail.com>
2023-11-28 13:45:21 +0300

    создала модели Genre, Category, Title и зарегистрировала в admin

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель Category."""
+    name = models.CharField(max_length=128, verbose_name='Название категории')
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+        def __str__(self):
+            return self.name
+
+
+class Genre(models.Model):
+    """Модель Genre."""
+    name = models.CharField(max_length=128, verbose_name='Название жанра')
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+        def __str__(self):
+            return self.name
+
+
+class Title(models.Model):
+    """Модель Title."""
+    name = models.CharField(max_length=128,
+                            verbose_name='Название произведения')
+    year = models.IntegerField(verbose_name='Год')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True,
+                                 related_name='categoryes')
+    genre = models.ManyToManyField(Genre, on_delete=models.SET_NULL,
+                                   null=True,)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+        def __str__(self):
+            return self.name

1b557d3e5d9835fa601efcd453b4b502559ce644
Святослав Поляков <viewtifulmadguy@gmail.com>
2023-11-29 14:58:42 +0300

    Удалил лишний код и добавил новое разрешение IsAdminOrReadOnly

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -19,5 +19,9 @@ class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_admin
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
+        )

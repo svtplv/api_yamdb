4ec5da86659eeb8b620de08e1be1e1c4055499af
Святослав Поляков <viewtifulmadguy@gmail.com>
2023-11-29 18:43:30 +0300

    убрал жанр из list_display TitleAdmin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -5,38 +5,47 @@ from .models import Category, Title, Genre, Review, Comment
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ('id',
-                    'name',
-                    'year',
-                    'category',
-                    'genre', )
+    list_display = (
+        'id',
+        'name',
+        'year',
+        'category',
+    )
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id',
-                    'name',
-                    'slug', )
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('id',
-                    'name',
-                    'slug', )
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('title',
-                    'text',
-                    'author',
-                    'score', )
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'score',
+    )
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('review',
-                    'text',
-                    'author',
-                    'pub_date', )
+    list_display = (
+        'review',
+        'text',
+        'author',
+        'pub_date',
+    )

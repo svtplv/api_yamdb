6e53d7d42a2c94482ae564778ab5dadaf2d49a79
Святослав Поляков <viewtifulmadguy@gmail.com>
2023-12-05 11:23:29 +0300

    добавил пустую строку между импортом в сериализаторах

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework import serializers
 from rest_framework.fields import CharField, EmailField
 from rest_framework.relations import SlugRelatedField
+
 from reviews.models import Category, Comment, Genre, Review, Title, User
 from users.validators import validate_username
 

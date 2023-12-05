2ee795299ed96a2c5634c585caab2f749003cfe3
Святослав Поляков <viewtifulmadguy@gmail.com>
2023-12-05 12:05:46 +0300

    вернул назад символ

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -8,7 +8,7 @@ def validate_username(value):
     if value in settings.FORBIDDEN_WORDS:
         raise ValidationError(f'Имя пользователя не может быть {value}')
 
-    unexpected_symbols = re.sub(r'[\w.@+-]', '', value)
+    unexpected_symbols = re.sub(r'[\w.@+-]+', '', value)
     if unexpected_symbols:
         raise ValidationError(
             'Имя пользователя не должно содержать следующих символов: '
